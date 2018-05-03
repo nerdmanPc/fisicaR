dfd0c3bc7f479656d24bcf3c1ce0209cd3615289
Pedro Costa <pedro@JuliaDebian>
2018-05-03 15:09:37 -0300

    nao tah pronto

---
+++ mru/code_mru.R
@@ -0,0 +1,4 @@
+data_mru[6, "Sensor1_s"] <- sum(data_mru[1:5, "Sensor1_s"]) / 5
+data_mru[6, "Sensor2_s"] <- sum(data_mru[1:5, "Sensor2_s"]) / 5
+data_mru[6, "Sensor3_s"] <- sum(data_mru[1:5, "Sensor3_s"]) / 5
+data_mru[6, "Sensor4_s"] <- sum(data_mru[1:5, "Sensor4_s"]) / 5
