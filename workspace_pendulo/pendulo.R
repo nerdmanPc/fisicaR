1e13e5030fc6c862ccc677ac9ddf450e4a4b762e
sauloandradegames <sauloandradegames@gmail.com>
2018-05-03 08:33:32 -0300

    Add lm(Pedro); A e w adicionados ao dataset

--- workspace_pendulo/pendulo.R
+++ workspace_pendulo/pendulo.R
@@ -1,45 +1,37 @@
-# Massa do pendulo (Kg)
-pendulo.massa <- 0.0671
+# ==============================
+# Calculos
+# ==============================
 
-# Angulo de inclinacao do pendulo (rad)
-theta <- (5*pi)/180
-
-# Valor da aceleracao da gravidade na UFBA
-G <- 9.7833
-
-# Banco de dados do experimento
-pendulo.data <- read.csv(file="pendulo.csv",sep=";")
-
-# Adicione uma coluna com as medias dos tempos
-pendulo.data$tmedia <- (pendulo.data$t1 + pendulo.data$t2 + pendulo.data$t3)/3
-
-# Funcao horaria do pendulo em funcao do tempo:
-# x(t) = A * cos(w*t + o) onde:
-# A: amplitude; w:vel.angular; o:var. mudanca de fase
-# Vamos construir as funcoes com base nas medidas da ultima iteracao
-# Tomando o maior comprimento de pendulo
-
-# Calculando amplitude
-# A = L * sin(theta)
-A <- pendulo.data$comprimento[10] * sin(theta)
-
-# Calculando velocidade angular
-# w = (2*pi)/T
-w = (2*pi)/pendulo.data$tmedia[10]
+G <- 9.7833 # Valor da aceleracao da gravidade na UFBA
+pendulo.massa <- 0.0671 # Massa do pendulo (Kg)
+pendulo.theta <- (5*pi)/180 # Angulo de inclinacao do pendulo (rad)
+pendulo.data <- read.csv(file="pendulo.csv",sep=";") # Banco de dados do experimento
+pendulo.data$tmedia <- (pendulo.data$t1 + pendulo.data$t2 + pendulo.data$t3)/15 # Adicione uma coluna com as medias dos tempos
+pendulo.data$tmedia2 <- pendulo.data$tmedia * pendulo.data$tmedia # Adicione uma coluna com os tempos linearizados
+pendulo.data$amplitude <- pendulo.data$comprimento * sin(pendulo.theta) # Adicione uma coluna com as amplitudes {A=L*sin(theta)}
+pendulo.data$omega <- (2*pi)/pendulo.data$tmedia # Adicione uma coluna com as velocidades angulares {w=(2*pi)/T}
 
 # Quanto a variavel de mudanca de fase
 # Vamos assumir um valor desprezivel (proxima de zero)
 
+# Realize a regressao linear T^2 vs. L
+pendulo.lm <- lm(pendulo.data$tmedia2 ~ pendulo.data$comprimento)
+
+# Funcao horaria do pendulo em funcao do tempo definida como:
+# x(t) = A * cos(w*t + o) onde:
+# A: amplitude; w:vel.angular; o:var. mudanca de fase
+# Vamos construir as funcoes com base nas medidas da ultima iteracao
+# Tomando o maior comprimento de pendulo (pendulo.data$comprimento[10])
+
 # De posse desses dados, podemos construir a funcao da posicao em funcao do tempo:
 pendulo.Posicao <- function(t){
-  return(A*cos(w*t))
+return(pendulo.data$amplitude[10]*cos(pendulo.data$omega[10]*t))
 }
 
 # Para obter as funcoes que descrevem a velocidade e a aceleracao em funcao do tempo
 # Pode-se derivar a funcao pendulo.Posicao()
 
-# No terminal do R, executou-se as linhas de codigo abaixo
-# para obtencao das derivadas.
+# No terminal do R, executou-se as linhas de codigo abaixo para obtencao das derivadas.
 # D(expression(A*cos(w*t)),"t")
 # D(expression(-(A*w*sin(w*t))),"t")
 
@@ -47,80 +39,95 @@ pendulo.Posicao <- function(t){
 # -(A * (sin(w * t) * w))
 # -(A * w * (cos(w * t) * w))
 
-# Numericamente as funcoes horarias sao:
-# x(t) = 0.0939*cos(0.7531*t)
-# v(t) = -0.0707*sin(0.7531*t)
-# a(t) = -0.0533*cos(0.7531*t)
-
 # Copiou-se e usou-se o resultado para construcao das outras funcoes:
 pendulo.Velocidade <- function(t){
-  return(-(A * (sin(w * t) * w)))
+return(-(pendulo.data$amplitude[10]*pendulo.data$omega[10]*sin(pendulo.data$omega[10]*t)))
 }
 
 pendulo.Aceleracao <- function(t){
-  return(-(A * w * (cos(w * t) * w)))
+return(-(pendulo.data$amplitude[10]*pendulo.data$omega[10]*pendulo.data$omega[10]*cos(pendulo.data$omega[10]*t)))
 }
 
-# Desenhe cada um dos graficos separadamente
+# A Energia Cinetica eh definida como Ec = (m*v^2)/2
+# Onde m:massa do pendulo; v:velocidade
+# Defina uma funcao para calcular a Energia Cinetica do pendulo em funcao do tempo
+pendulo.Ecin <- function(t){
+vel <- pendulo.Velocidade(t)
+return((pendulo.massa*(vel^2))/2)
+}
+
+# A energia potencial eh definida como Ep = m*g*h
+# Onde m:massa do pendulo; g:aceleracao da gravidade: h:altura do pendulo em relacao ao referencial (geralmente, o chao)
+# A altura h eh composta de dois componentes:
+# * h0: a altura do pendulo em relacao ao solo quando em repouso
+# * li: a altura do pendulo em relacao ao solo quando peturbado (i.e, quando deslocado para o inicio do movimento)
+# Dai h = h0 + li
+# O referencial usado foi um ponto onde, quando em repouso, o pendulo se encontra muito proximo do solo
+# Nesse caso, h0 -> 0 (h0 eh um valor muito proximo de zero e, portanto, desprezivel)
+# Portanto h = h0. Dai
+# Ep = m*g*li, onde li = L*(1-cos(theta)), onde L representa o comprimento do pendulo e theta o angulo de perturbacao
+# Substituindo li na formula temos, enfim, que:
+# Ep = m*g*L*(1-cos(theta))
+
+# Durante todo o movimento, o angulo do pendulo varia em funcao do tempo e da velocidade angular do mesmo
+# Com isso, defina uma funcao para calcular a Energia Potencial do pendulo em funcao do tempo
+# (Agradecimentos a Alisson, Fabio, Giusepe e Lucas pela ideia da funcao)
+pendulo.Epot <- function(t){
+var_theta <- pendulo.theta*cos(pendulo.data$omega[10]*t)
+return(pendulo.massa*G*pendulo.data$comprimento[10]*(1-cos(var_theta)))
+}
+
+# ==============================
+# Desenho dos graficos
+# ==============================
+
+# Dados obtidos: T x L
+jpeg("pendulo_TxL.png")
+plot(pendulo.data$comprimento, pendulo.data$tmedia, main="Pêndulo: Comprimento x Período", xlab="L(m)", ylab="T(s)")
+dev.off()
+
+# Dados obtidos: Regressao linear
+jpeg("pendulo_lm.png")
+plot(pendulo.data$comprimento, pendulo.data$tmedia2, main="Pêndulo: Comprimento x Período²", xlab="L(m)", ylab="T²(s²)")
+abline(pendulo.lm)
+dev.off()
+
+# Posicao em funcao do tempo
 jpeg("pendulo_posicao.png")
-plot(pendulo.Posicao, col="red", main="Pêndulo: Posição x Tempo", xlab="Tempo(s)", ylab="Posição(m)", xlim=c(0,10))
+plot(pendulo.Posicao, col="red", main="Pêndulo: Posição x Tempo", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Posição(m)", xlim=c(0,3))
 dev.off()
 
+# Velocidade em funcao do tempo
 jpeg("pendulo_velocidade.png")
-plot(pendulo.Velocidade, col="green", main="Pêndulo: Velocidade x Tempo", xlab="Tempo(s)", ylab="Velocidade(m/s)", xlim=c(0,10))
+plot(pendulo.Velocidade, col="green", main="Pêndulo: Velocidade x Tempo", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Velocidade(m/s)", xlim=c(0,3))
 dev.off()
 
+# Aceleracao em funcao do tempo
 jpeg("pendulo_aceleracao.png")
-plot(pendulo.Aceleracao, col="blue", main="Pêndulo: Aceleração x Tempo", xlab="Tempo(s)", ylab="Aceleração(m/s²)", xlim=c(0,10))
+plot(pendulo.Aceleracao, col="blue", main="Pêndulo: Aceleração x Tempo", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Aceleração(m/s²)", xlim=c(0,3))
 dev.off()
 
-# Desenhe os graficos das tres funcoes simultaneamente
+# Posicao + Velocidade + Aceleracao
 jpeg("pendulo_funcoes.png")
-plot(pendulo.Posicao, col="red", main="Pêndulo: Sobreposicao Posição, Velocidade e Aceleração", xlab="Tempo(s)", ylab="Posição(m) | Velocidade(m/s) | Aceleração(m/s²)", xlim=c(0,10))
-plot(pendulo.Velocidade, col="green",  xlim=c(0,10), add=T)
-plot(pendulo.Aceleracao, col="blue",  xlim=c(0,10), add=T)
+plot(pendulo.Posicao, col="red", main="Pêndulo: Posição, Velocidade e Aceleração", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Posição(m) | Velocidade(m/s) | Aceleração(m/s²)", yaxt='n', xlim=c(0,3), ylim=c(-1.5,1.5))
+plot(pendulo.Velocidade, col="green",  xlim=c(0,3), add=T)
+plot(pendulo.Aceleracao, col="blue",  xlim=c(0,3), add=T)
 legend("bottomright", c("Posição", "Velocidade", "Aceleração"), cex=0.8, col=c("red", "green", "blue"), lty=1)
 dev.off()
 
-# Executando summary() para Posicao, Velocidade e Aceleracao nos tempos t=0:10 tem-se que:
-# max_pendulo.Posicao = 0.09390m (Amplitude do movimento)
-# max_pendulo.Velocidade = 0.069377m/s (Velocidade maxima do movimento)
-# max_pendulo.Aceleracao = 0.052856m/s^2 (Maxima aceleracao do movimento)
-
-# Defina uma funcao para calcular a Energia Cinetica do pendulo em funcao do tempo
-pendulo.Ecin <- function(t){
-  vel <- pendulo.Velocidade(t)
-  return ((pendulo.massa*(vel^2))/2)
-}
-
-# Com o angulo theta e a velocidade maxima
-# podemos extrair a energia potencial maxima e a energia cinetica maxima
-# Quanto a energia potencial, tomamos como referencia um ponto extremamente proximo do solo
-# quando o pendulo se encontra em repouso. Sejam:
-# * h0 a altura do pendulo em relacao ao solo quando em repouso
-# * Li a altura do pendulo em relacao ao solo quando distendido
-# Nesse referencial, h0 -> 0
-# Com isso, Epot = m*g*h, onde h = Li + ho
-# Como h0 ->0 tempos Epot = m*g*Li
-# Conforme mostrado em sala, Li=L*(1-cos(theta)), portanto
-# Epot = m*g*L*(1-cos(theta))
-
-# Em relacao aos valores da 10a iteracao do experimento
-# Energia potencial maxima do sistema:
-pendulo.maxEpot <- pendulo.massa * G * pendulo.data$comprimento[10] * (1 - cos(theta))
-# Energia cinetica maxima do sistema:
-pendulo.maxEcin <- (pendulo.massa * (0.069377^2))/2
-
-# Imprima na tela os valores
-print("pendulo.maxEpot")
-print(pendulo.maxEpot)
-print("pendulo.maxEcin")
-print(pendulo.maxEcin)
-
-# Desenhe o grafico da energia cinetica
+# Energia Cinetica
 jpeg("pendulo_ecin.png")
-plot(pendulo.Ecin, col="red", main="Pêndulo: Energia Cinética x Tempo", xlab="Tempo(s)", ylab="Energia Cinética(J)", xlim=c(0,10))
+plot(pendulo.Ecin, col="red", main="Pêndulo: Energia Cinética x Tempo", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Energia Cinética(J)", xlim=c(0,3))
 dev.off()
 
-# Duvida para discussao:
-# Discrepancia entre valores de energia: porque nao sao iguais? Ha algo errado?
+# Energia Potencial
+jpeg("pendulo_Epot.png")
+plot(pendulo.Epot, col="green", main="Pêndulo: Energia Potencial x Tempo", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Energia Potencial(J)", xlim=c(0,3))
+dev.off()
+
+# Variacao da energia do pendulo
+jpeg("pendulo_Energia.png")
+plot(pendulo.Ecin, col="red", main="Pêndulo: Variação da Energia do Sistema", sub=expression(paste(omega, "=3.765393")), xlab="Tempo(s)", ylab="Energia(J)", xlim=c(0,3), ylim=c(0,0.005))
+plot(pendulo.Epot, col="green", xlim=c(0,3), ylim=c(0,0.005), add=T)
+legend("topright", c("Energia Cinética", "Energia Potencial"), cex=0.8, col=c("red", "green"), lty=1)
+dev.off()
