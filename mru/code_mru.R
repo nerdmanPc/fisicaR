beef713c287ae39b17407a71d307c0ea652b94fc
Pedro Costa <pedro@JuliaDebian>
2018-05-04 17:28:51 -0300

    Regressao pronta e velocidade mdia obtida

--- mru/code_mru.R
+++ mru/code_mru.R
@@ -1,10 +1,14 @@
+# Importar dataset "data_mru.csv"
+
 data_mru[6, "Sensor1_s"] <- sum(data_mru[1:5, "Sensor1_s"]) / 5
 data_mru[6, "Sensor2_s"] <- sum(data_mru[1:5, "Sensor2_s"]) / 5
 data_mru[6, "Sensor3_s"] <- sum(data_mru[1:5, "Sensor3_s"]) / 5
 data_mru[6, "Sensor4_s"] <- sum(data_mru[1:5, "Sensor4_s"]) / 5
 
-tempos <- c(data_mru[6, 1:4])
-posicoes <- c(data_mru[1, 5:8])
+tempos <- unlist(data_mru[6, 1:4])
+posicoes <- unlist(data_mru[1, 5:8])
+regressao <- lm(formula = posicoes ~ tempos)
+velocidade <- regressao$coefficients[2]
 
-plot(tempos, posicoes, main="Função Horária", xlab="Tempo (s)", ylab="Posição (s)")
-abline(lm(c(tempos, posicoes)))
+plot(tempos, posicoes, main="Função Horária", xlab="Tempo (s)", ylab="Posição (m)")
+abline(regressao)
